e59b18df8384aa6baa4fa2a9056d51664582c328
naa091 <naa09136@gmail.com>
2024-12-14 09:14:40 +0300

    Create project

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Александр Нистратов on 14.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
