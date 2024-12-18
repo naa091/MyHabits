16d4dee6a1345f5a3dc3cbc974216c7bd9b8b63c
naa091 <naa09136@gmail.com>
2024-12-18 09:55:35 +0300

    Add UIComponents

---
+++ MyHabits/UIComponents/AppColor/AppColor.swift
@@ -0,0 +1,18 @@
+//
+//  AppColor.swift
+//  MyHabits
+//
+//  Created by Александр Нистратов on 17.12.2024.
+//
+import UIKit
+
+enum AppColor {
+    static let appDarkGrey = UIColor.systemGray
+    static let appLightGrey = UIColor.systemGray2
+    static let appSnowGrey = UIColor(hexString: "F2F2F7")
+    static let appAmethystPurple = UIColor(hexString: "A116CC")
+    static let appBrightBlue = UIColor(hexString: "296DFF")
+    static let appLimeGreen = UIColor(hexString: "1DB322")
+    static let appIndigoBlue = UIColor(hexString: "6236FF")
+    static let appPeachOrange = UIColor(hexString: "FF9F4F")
+}
